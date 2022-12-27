0a8c02be2677c686277a9af86dee03a842d3b569
DJY <656756130@qq.com>
2022-12-27 09:56:46 +0800

    add the package

--- package/random_networks.py
+++ package/random_networks.py
@@ -15,3 +15,16 @@ def rand_edge(G, vi, vj, p=0.8):  # 默认概率p=0.1
         print(probability)
         G.add_edge(vi, vj)
     return G
+
+def create_random_networks(G,nodes_number,p=0.8):
+    i = 0
+    while i<nodes_number:
+        j = 0
+        while j<nodes_number:
+            if j<i:
+                j+=1
+            else:
+                rand_edge(G=G,vi=i,vj=j,p=p)
+                j+=1
+        i+=1
+    return G
